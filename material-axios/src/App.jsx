092768d0c87d321d37730effae7c9943bb1a7a0d
tuanndph16237 <tuanndph16237@example.com>
2023-04-18 13:01:11 +0700

    material-news

---
+++ material-axios/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import NavMenu from './Component/NavMenu/NavMenu';
+import NewsData from './Component/NewsData/NewsData.jsx';
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className="container mx-auto max-w-screen-xl py-4">
+        <NavMenu />
+
+        <NewsData/>
+      </div>
+    </div>
+  )
+}
+
+export default App;
