08928c6f5fcd740cec66200f8735e30513e07c25
tuanndph16237 <tuanndph16237@example.com>
2023-08-17 01:43:30 +0700

    fitness template

--- legened-landing-page/src/App.jsx
+++ legened-landing-page/src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
 import 'swiper/css'
-import 'swiper/css/pagination'
-import 'swiper/css/effect-fade'
-import './App.scss'
+import 'swiper/css/pagination';
+import 'swiper/css/effect-fade';
+import './App.scss';
 
-import Header from './components/header/Header'
-import Home from './pages/Home'
+import Header from './components/header/Header';
+import Home from './pages/Home';
 
 function App() {
   const [count, setCount] = useState(0)
