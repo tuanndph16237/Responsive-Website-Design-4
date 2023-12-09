71bf4a9ec369bdf7a88ab3d4eccedf6ba9647f2b
tuanndph16237 <tuanndph16237@example.com>
2023-12-09 18:33:16 +0700

    blog with strapi

---
+++ react-tailwind-blog/src/components/index.js
@@ -0,0 +1,6 @@
+import Navbar from "./Navbar";
+import Blogs from "./Blogs";
+import Footer from "./Footer";
+import BlogContent from "./BlogContent";
+
+export { Navbar, Blogs, Footer, BlogContent };
